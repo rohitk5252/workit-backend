cc56b189604a7b25a2caca2dd8e2e3338a1e7fab
navjot0 <rohitofficial2001@gmail.com>
2023-03-18 14:06:03 +0530

    spelling mistake

--- models/userModel.js
+++ models/userModel.js
@@ -28,7 +28,7 @@ userSchema.statics.signup = async function(email, password) {
     }
 
     if(!validator.isStrongPassword(password)) {
-        throw Error('Password not strong emough')
+        throw Error('Password not strong enough')
     }
 
     const exists = await this.findOne({ email })
